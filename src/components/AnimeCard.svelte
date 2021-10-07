9fdb1c1c828270a0a531c3c32a2a09bd77994417
Pei-Ming Wu <p408865@gmail.com>
2021-10-07 23:50:37 +0800

    feat: add plus button

--- src/components/AnimeCard.svelte
+++ src/components/AnimeCard.svelte
@@ -2,33 +2,42 @@
     import { Anime } from "../anime";
 
     export let anime: Anime;
-    let isHover = false;
-
 </script>
 
-<div class="card"
-     on:mouseenter={() => isHover = true}
-     on:mouseleave={() => isHover = false}
-     class:hover={isHover}
->
-    <div class="cover">
-        <img src={anime?.images[0].getThumbnailUrl()} alt={anime?.name?.japanese} loading="lazy" />
+<div class="wrapper">
+    <img class="plus" src="https://img.icons8.com/color/24/000000/plus--v1.png" alt="icon"/>
+    <div class="card">
+        <div class="cover">
+            <img src={anime?.images[0].getThumbnailUrl()} alt={anime?.name?.japanese} loading="lazy" />
+        </div>
+        <p class="title">{anime?.name?.chinese}</p>
     </div>
-    <p class="title">{anime?.name?.chinese}</p>
 </div>
 
 <style>
+    .wrapper {
+        position: relative;
+    }
+
+    .plus {
+        position: absolute;
+        right: 0;
+        z-index: 999;
+    }
+
+    .plus:hover {
+        cursor: pointer;
+        opacity: 0.7;
+    }
+
     .card {
+        position: relative;
         display: flex;
         justify-content: space-between;
         flex: 1 1;
         width: 200px;
         align-items: center;
         gap: 5px;
-    }
-
-    .card:hover {
-        cursor: pointer
     }
 
     .card:hover > .cover {
