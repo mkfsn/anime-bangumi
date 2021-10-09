d4a52f9f5b091b106e15ac090e715706c1991ead
Pei-Ming Wu <p408865@gmail.com>
2021-10-09 22:23:02 +0800

    feat: add icon-button to github repo

--- src/components/TopBar.svelte
+++ src/components/TopBar.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
-    import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+    import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
     import IconButton from '@smui/icon-button';
 
     export let openSideBar;
     let today = new Date();
+
+    const goToGithub = () => {
+        window.open("https://github.com/mkfsn/anime-bangumi", '_blank').focus();
+    }
 </script>
 
 <TopAppBar variant="static" prominent={false} color="secondary" class="top-app-bar">
     <Row>
         <Section>
-            <IconButton class="material-icons" on:click={() => openSideBar = !openSideBar}>menu</IconButton>
+            {#if openSideBar}
+                <IconButton class="material-icons" on:click={() => openSideBar = !openSideBar}>menu_open</IconButton>
+            {:else}
+                <IconButton class="material-icons" on:click={() => openSideBar = !openSideBar}>menu</IconButton>
+            {/if}
             <Title>アニメ番組</Title>
             <span class="today">
                 {today.toLocaleDateString('zh-tw', {
@@ -24,6 +32,7 @@
         <Section/>
 
         <Section align="end">
+            <IconButton class="material-icons" aria-label="Github Repository" on:click={goToGithub}>source</IconButton>
         </Section>
     </Row>
 </TopAppBar>
