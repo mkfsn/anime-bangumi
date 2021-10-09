d6e129b2d6c90067ef22d0057643cd4a16045d36
Pei-Ming Wu <p408865@gmail.com>
2021-10-09 18:19:51 +0800

    feat: show day-of-week and day-of-month

--- src/components/Timetable.svelte
+++ src/components/Timetable.svelte
@@ -8,10 +8,38 @@
         [], [], [], [], [], [], []
     ]
 
+    const daysOfCurrentWeek = getDaysOfCurrentWeek(getStartDateOfWeek());
+
+    function isToday(dayOfWeek: number): boolean {
+        const today = new Date();
+        return today.getDay() === dayOfWeek;
+    }
+
     function nameOfDay(v: number): string {
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        console.assert(v >= 0 && v <= days.length);
-        return days[v];
+        console.assert(v >= 0 && v <= daysOfCurrentWeek.length);
+        return daysOfCurrentWeek[v].toLocaleString("zh-tw", {weekday: "short"})
+    }
+
+    function dayOfMonth(v: number): string {
+        console.assert(v >= 0 && v <= daysOfCurrentWeek.length);
+        return daysOfCurrentWeek[v].getDate().toString();
+    }
+
+    function getStartDateOfWeek(): Date {
+        let result = new Date();
+        const day = result.getDay()
+        result.setHours(result.getHours() - day * 24)
+        return result;
+    }
+
+    function getDaysOfCurrentWeek(startDateOfWeek: Date): Date[] {
+        const results: Date[] = [];
+        for (let i = 0; i < 7; i++) {
+            const t = new Date(startDateOfWeek);
+            t.setHours(t.getHours() + i * 24);
+            results.push(t);
+        }
+        return results;
     }
 
     const unsubscribe = watchlist.subscribe(animeList => {
@@ -38,7 +66,10 @@
     {#each animeGroups as animeList, weekday}
         <div class="weekday">
             <div class="header">
-                <span>{nameOfDay(weekday)}</span>
+                <div class="date" class:today={isToday(weekday)}>
+                    <p>{nameOfDay(weekday)}</p>
+                    <p>{dayOfMonth(weekday)}</p>
+                </div>
             </div>
             {#each animeList as anime}
                 <AnimeCard anime={anime}/>
@@ -64,7 +95,32 @@
     .header {
         text-align: center;
         justify-self: center;
-        padding-bottom: .5em;
+        padding-top: 3px;
+        padding-bottom: 3px;
         border-bottom: 1px solid #dfdfdf;
+    }
+
+    .date > p {
+        margin: 0;
+    }
+
+    .date {
+        border-radius: 100%;
+        width: 40px;
+        padding: 5px;
+        margin: 0 auto;
+    }
+
+    .date:hover {
+        background-color: #eaeaea;
+    }
+
+    .date.today {
+        background-color: #6292e9;
+        color: #fcfcfc;
+    }
+
+    .date.today:hover {
+        background-color: #3569c8;
     }
 </style>
