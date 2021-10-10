2276eadf0bbb5ac1d48c72ea02d1253f683a109d
Pei-Ming Wu <p408865@gmail.com>
2021-10-10 13:22:47 +0800

    refactor: add function: loadAnimeListFromUrl

--- src/App.svelte
+++ src/App.svelte
@@ -2,45 +2,23 @@
     import Timetable from "./components/Timetable.svelte";
     import Sidebar from "./components/Sidebar.svelte";
     import TopBar from "./components/TopBar.svelte";
-    import {onMount} from "svelte";
-    import {Anime} from "./models/anime";
+    import { onMount } from "svelte";
+    import { Anime } from "./models/anime";
     import { watchlist } from './stores/watchlist';
-
-    const fetchYear = 2021, fetchMonth = 10;
+    import { loadAnimeListFromUrl } from "./utils/url";
 
     let openSideBar = false;
-
     let animeList = [];
 
     onMount(async () => {
+        const fetchYear = 2021, fetchMonth = 10;
 
         const res = await fetch(`/assets/${fetchYear}/${fetchMonth}.json`);
         for (let result of await res.json()) {
-            animeList.push(new Anime(result, fetchYear, fetchMonth))
+            animeList.push(new Anime(result, fetchYear, fetchMonth));
         }
 
-        const hash = window.location.hash.slice(1);
-        if (hash.startsWith("/v1")) {
-            const groups = hash.slice(4).split("/");
-            groups.forEach(group => {
-                const [yearMonth, animeIDs] = group.split("=", 2)
-
-                const year = parseInt(yearMonth.slice(0, 4)),
-                      month = parseInt(yearMonth.slice(4));
-
-                if (!(year === fetchYear && month === fetchMonth)) {
-                    return;
-                }
-
-                if (animeIDs === "") {
-                    return;
-                }
-
-                const animeIDSet = new Set<String>(animeIDs.split(","))
-                const sharedAnimeList = animeList.filter(anime => animeIDSet.has(anime.id));
-                $watchlist = [... sharedAnimeList];
-            })
-        }
+        $watchlist = loadAnimeListFromUrl(animeList, fetchYear, fetchMonth);
     });
 </script>
 
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,5 +1,10 @@
 import type { Anime } from "../models/anime";
 
+const version = "v1";
+const sectionDelimiter = "/";
+const idDelimiter = ",";
+const keyValueDelimiter = "=";
+
 function storeAnimeListToUrl(animeList: Anime[]) {
     const animeGroups = {};
 
@@ -13,16 +18,44 @@ function storeAnimeListToUrl(animeList: Anime[]) {
         animeGroups[yearMonth].push(anime.id);
     });
 
-    let newHash = "/v1";
+    let newHash = `${sectionDelimiter}${version}`;
 
     for (let yearMonth in animeGroups) {
-        const animeIDs = animeGroups[yearMonth].join(",");
-        newHash += `/${yearMonth}=${animeIDs}`;
+        const animeIDs = animeGroups[yearMonth].join(idDelimiter);
+        newHash += `${sectionDelimiter}${yearMonth}${keyValueDelimiter}${animeIDs}`;
     }
 
     window.location.hash = newHash;
 }
 
+function loadAnimeListFromUrl(allAnimeList: Anime[], fetchYear: number, fetchMonth: number): Anime[] {
+    const hash = window.location.hash.slice(1);
+    if (!hash.startsWith(`${sectionDelimiter}${version}`)) {
+        return [];
+    }
+
+    const groups: string[] = hash.slice(4).split(sectionDelimiter);
+    let animeList: Anime[] = [];
+
+    groups.forEach(group => {
+        const [animeYearMonth, animeIDs] = group.split(keyValueDelimiter, 2)
+
+        const animeYear = parseInt(animeYearMonth.slice(0, 4)),
+              animeMonth = parseInt(animeYearMonth.slice(4));
+
+        if (!(animeYear === fetchYear && animeMonth === fetchMonth) || animeIDs === "") {
+            return;
+        }
+
+        const animeIDSet = new Set<String>(animeIDs.split(idDelimiter));
+        const sharedAnimeList = allAnimeList.filter(anime => animeIDSet.has(anime.id));
+        animeList = [... sharedAnimeList];
+    })
+
+    return animeList;
+}
+
 export {
-    storeAnimeListToUrl
+    storeAnimeListToUrl,
+    loadAnimeListFromUrl,
 }
